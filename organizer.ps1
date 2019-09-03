32380b92c3dfeb4ca6a56d86e081e9943d091940
Nicolás de Ory <deorynicolas@gmail.com>
2019-09-03 17:08:56 +0200

    Initial commit

---
+++ organizer.ps1
@@ -0,0 +1,39 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$inputpath,
+    [Parameter(Mandatory=$true)][string]$outputpath
+) 
+
+$i = 0
+
+$items = Get-ChildItem -Path $inputpath -Recurse
+
+$items | ForEach-Object {
+    if ( -not $_.PSIsContainer)
+    {
+        $monthString = $_.LastWriteTime.Month
+        $yearString = $_.LastWriteTime.Year
+        $outputFolder = $outputpath + "\$($yearString)-$($monthString)"
+        if ( -not (Test-Path $outputFolder)) 
+        {
+            md $outputFolder | Out-Null
+        }
+
+        $folderFiles  = @(Get-ChildItem $outputFolder -File -Filter $_.Name | Select-Object -ExpandProperty Name)
+
+        $baseName = $_.BaseName
+        $extension = $_.Extension
+        $newName = $baseName + $extension
+        $count = 1
+        while ($folderFiles -contains $newName) {
+            # check for duplicated files, do not overwrite
+            $newName = "{0}({1}){2}" -f $baseName, $count++, $extension
+        }
+
+        Copy-Item -Path $_.FullName -Destination "$($outputFolder)\\$($newName)"
+        $i++
+        Write-Progress -Activity $_.FullName -PercentComplete (($i / $items.count)*100)
+           
+    }
+        
+}
+Write-Host Done!
